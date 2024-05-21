bd451d81d47a0b77b5cb92ef91e855d45e2ead4f
Gimenes <f.gimenes1316@gmail.com>
2024-05-21 15:35:46 -0300

    tentantoAddModal

--- pages/compras.js
+++ pages/compras.js
@@ -1,22 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Modal } from 'react-native';
+import { ModalTokens } from '../components/modal';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useState } from "react";
 import { useNavigation } from '@react-navigation/native';
 
 
 export default function Compras() {
 
-  const navigation=useNavigation()
+  const navigation = useNavigation()
 
-  function mudarPag(){
+  const [telaModal, configTelaModal] = useState(false)
+
+
+  function abrirModal() {
+    configTelaModal(true);
+  }
+
+  function mudarPag() {
     navigation.navigate('index')
-  } 
+  }
 
   return (
     <LinearGradient
       colors={['#FFFFFF', '#FCE8B2']}
       style={styles.container}>
+        
+      <Modal visible={telaModal} animationType="fade" transparent={true}>
+        <ModalTokens fechar={() => configTelaModal(false)} />
+      </Modal>
+
       <View style={styles.header}>
         <TouchableOpacity onPress={mudarPag}>
           <Icon name="caret-left" size={60} color="#404E4D" />
@@ -26,7 +40,12 @@ export default function Compras() {
           <Icon name="search" size={35} color="#404E4D" />
         </TouchableOpacity>
       </View>
-      <Text>compras</Text>
+      <TouchableOpacity style={styles.BotaoAnterior} onPress={abrirModal}>
+        <View style={styles.conteudoBotoes}>
+          <Text style={styles.compraAnterior}>Clique e adicione o produto</Text>
+          <Icon name="plus-square" size={30} color="#545454" />
+        </View>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </LinearGradient>
 
@@ -38,6 +57,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-start',
+    gap: 50,
   },
   header: {
     width: '100%',
@@ -45,8 +65,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-around',
   },
-  imageHeader:{
+  imageHeader: {
     width: 50,
     height: 80,
+  },
+  BotaoAnterior: {
+    marginTop: 10,
+    width: 300,
+    height: 71,
+    backgroundColor: '#FFC030',
+    borderRadius: 15,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  compraAnterior: {
+    color: '#545454',
+    fontSize: 15,
+    marginRight: 20,
+  },
+  conteudoBotoes: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
