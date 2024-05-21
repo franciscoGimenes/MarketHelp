fd0a5929ef489b5b9ea48b7457826b1efc0e9695
Gimenes <f.gimenes1316@gmail.com>
2024-05-21 15:07:12 -0300

    addHeader

--- pages/compras.js
+++ pages/compras.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from '@react-navigation/native';
+
 
 export default function Compras() {
+
+  const navigation=useNavigation()
+
+  function mudarPag(){
+    navigation.navigate('index')
+  } 
+
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={['#FFFFFF', '#FCE8B2']}
+      style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={mudarPag}>
+          <Icon name="caret-left" size={60} color="#404E4D" />
+        </TouchableOpacity>
+        <Image source={require('../assets/logoMarketHelp2.png')} style={styles.imageHeader} />
+        <TouchableOpacity>
+          <Icon name="search" size={35} color="#404E4D" />
+        </TouchableOpacity>
+      </View>
       <Text>compras</Text>
       <StatusBar style="auto" />
-    </View>
+    </LinearGradient>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  imageHeader:{
+    width: 50,
+    height: 80,
   },
 });
